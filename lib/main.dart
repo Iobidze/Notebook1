7306b56aec7cb6a975a3da71c6ce43b6d8f51aca
FedorP121 <fedor121@inbox.ru>
2023-01-02 02:19:14 +0300

    fix1

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,28 @@
 import 'package:flutter/material.dart';
-import 'package:riverpod/riverpod.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() {
-  runApp(const ProviderScope(child: MyApp(fhfghfghfgh)));
+  runApp(ProviderScope(child: Notebook()));
+}
+
+// final String a = '10';
+
+final ProvaiderName = Provider(
+  (ref) {
+    return 'asdasd';
+  },
+);
+
+class Notebook extends ConsumerWidget {
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    final name = ref.watch(ProvaiderName);
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text(name),
+        ),
+      ),
+    );
+  }
 }
